8cc13e8b099be30e3e1641bd5c8c09f3956b2534
dehan <dehandecroos@gmail.com>
2015-06-12 19:31:02 +0530

    Initial Commit

---
+++ MacFypCode/Dehan/XcodeMacFypProject/XcodeMacFypProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeMacFypProject
+//
+//  Created by Dehan on 6/12/15.
+//  Copyright (c) 2015 nephelium. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
