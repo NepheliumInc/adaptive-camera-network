8054ddd56247b0f2eb160261538789bc47db5cc9
jdc91 <dehandecroos@gmail.com>
2015-09-06 12:15:48 +0530

    Rest
    Rest

--- BackupCode/main.cpp
+++ BackupCode/main.cpp
@@ -1,7 +1,7 @@
 #include "qttest3.h"
 
 #include <QtWidgets/QApplication>
-#include <QLabel>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
